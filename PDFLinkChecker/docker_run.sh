805fe76d81753ffd4fb88554ef555d5d883c73bf
Sahil-Singamsetty <JustSahil-S@users.noreply.github.com>
2024-09-22 16:27:13 -0700

    Fixes #30

--- PDFLinkChecker/docker_run.sh
+++ PDFLinkChecker/docker_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEF_INST_UIPORT="8000"
+DEF_INST_UIPORT="80"
 DEF_DATA_DIR="/DATA"
 DEF_PM2_OPTIONS="-m -s"
 
@@ -8,7 +8,7 @@ show_usage() {
     cat << EOF
 
 USAGE:
-docker run -ti -v <host_dir>:/DATA [-p <UI_port>:2000] [--name="cnt_name"] <docker_image><:tag>
+docker run -ti -v <host_dir>:/DATA -p <UI_port>:8000 [--name="cnt_name"] <docker_image><:tag>
 
 host_dir:  directory for container data. Use an existing directory for state persistency through upgrades
 ui_port:   any unused TCP port for UI (optional)
@@ -16,8 +16,8 @@ cnt_name:  container name, same as inst_name preferred (optional)
 tag:       optional tag
 
 EXAMPLES:
-docker run -ti -v ~/my_data:/DATA  broken_link_checker:latest
-docker run -ti -v ~/my_data:/DATA  --name "django_app"  broken_link_checker:latest
+docker run -ti -v ~/my_data:/DATA -p 80:80 broken_link_checker:latest
+docker run -ti -v ~/my_data:/DATA -p 80:8000 --name "django_app"  broken_link_checker:latest
 
 EOF
 }
@@ -59,24 +59,24 @@ else
     python manage.py runserver 0.0.0.0:$INST_UIPORT
 fi
 
-:'
-if [ -z "$PM2_OPTIONS" ]
-then
-    PM2_OPTIONS=$DEF_PM2_OPTIONS
-fi
-if [ -f $DATA_DIR/pm2_$INST_ID.json ]
-then
-    # enable redis and python server if necessary
-    if [ ! -z "$PYTHON_SERVER" ]
+if 0
+    if [ -z "$PM2_OPTIONS" ]
     then
-        redis-server &
-        pm2 start $ERED_DIR/bin/eREDServer.py $PM2_OPTIONS --name ered_py
+        PM2_OPTIONS=$DEF_PM2_OPTIONS
     fi
-    pm2 start $DATA_DIR/pm2_$INST_ID.json $PM2_OPTIONS --name ered
-    pm2 logs
-else
-    echo "pm2_$INST_ID.json" file not found!!!
-    echo "exiting"
-    exit 1
+    if [ -f $DATA_DIR/pm2_$INST_ID.json ]
+    then
+        # enable redis and python server if necessary
+        if [ ! -z "$PYTHON_SERVER" ]
+        then
+            redis-server &
+            pm2 start $ERED_DIR/bin/eREDServer.py $PM2_OPTIONS --name ered_py
+        fi
+        pm2 start $DATA_DIR/pm2_$INST_ID.json $PM2_OPTIONS --name ered
+        pm2 logs
+    else
+        echo "pm2_$INST_ID.json" file not found!!!
+        echo "exiting"
+        exit 1
+    fi
 fi
-'
